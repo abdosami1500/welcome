b390ca2f64ef29d6e8ba8c9a1106ba69e2d1b09c
abdosami1500 <44530940+abdosami1500@users.noreply.github.com>
2018-11-22 21:02:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,33 @@
-new Discord.Client();
-console.log("Scrpit By Dream");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 
-client.on("ready", () => {
-let channel =     client.channels.get("505125121896480780")
-setInterval(function() {
-channel.send(`youtube`);
-}, 25)
-})
- 
- 
-client.login('NTAzMjY3MDE0NDUwNjEwMjE3.DrYFXA.AfTopE0czjijO_HgM4rM3RuIBrg');
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return; 
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر ي قلبي' , `Welcome to the server, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                      
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter("**Last Code**")
+        .setTimestamp()
+    
+      channel.sendEmbed(embed);
+    });
+
+
+client.login('NTE0MTIyMzk1NTQwMjU4ODE3.DtR9gA.7Oeq66BZLlh_dTlR3zodlUAxLxE');]
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
